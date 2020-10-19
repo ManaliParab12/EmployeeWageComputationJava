353e2452be1ee8ed055451996673bd4b31746860
ManaliParab12 <manaliparab10@gmail.com>
2020-10-19 14:17:20 +0530

    Added Part Time Employee Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,11 +2,14 @@ public class EmployeeWageComputation {
 
    public static void main(String args[]){
       int IS_FULL_TIME = 1;
+      int IS_PART_TIME = 2;
       int EMP_RATE_PER_HR = 20;
       int empHrs;
       int dailyWage;
-      double empCheck=Math.floor(Math.random()*10)%2;
-      if (IS_FULL_TIME == 1 )
+      double empCheck=Math.floor(Math.random()*10)%3;
+      if ( empCheck == IS_PART_TIME )
+          empHrs = 4;
+      else if ( empCheck == IS_FULL_TIME )
           empHrs = 8;
       else
           empHrs = 0;
