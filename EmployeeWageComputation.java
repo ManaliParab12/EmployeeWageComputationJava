c9c99840f1f7289339a4a8c354d9d8b3392ec27e
ManaliParab12 <manaliparab10@gmail.com>
2020-10-19 14:51:15 +0530

    Calculated Total Wages for a month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,22 +2,29 @@ public class EmployeeWageComputation {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HR = 20;
+    public static final int NUM_OF_WORKING_DAYS =20;
 
     public static void main(String args[]){
         int empHrs;
         int dailyWage;
-        int empCheck = (int) Math.floor(Math.random()*10)%3;
-        switch (empCheck) {
-            case IS_PART_TIME :
-                 empHrs = 4;
-                 break;
-            case IS_FULL_TIME :
-                 empHrs = 8;
-                 break;
-            default :
-                 empHrs = 0;
+        int monthlyWage = 0;
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+            int empCheck = (int) Math.floor(Math.random()*10)%3;
+            switch (empCheck) {
+                case IS_PART_TIME :
+                     empHrs = 4;
+                     break;
+                case IS_FULL_TIME :
+                     empHrs = 8;
+                     break;
+                default :
+                     empHrs = 0;
+            }
+            dailyWage = empHrs * EMP_RATE_PER_HR;
+            monthlyWage += dailyWage;
+            System.out.println("Daily Employee Wage : " + dailyWage);
         }
-        dailyWage = empHrs * EMP_RATE_PER_HR;
-        System.out.println("Daily Employee Wage : " + dailyWage);
-   }
+        System.out.println("Monthly Employee Wage : " + monthlyWage);
+    }
+
 }
