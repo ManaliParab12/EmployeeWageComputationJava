6237fa363973f71b694b7e59e3e4c66dcb8bb7da
ManaliParab12 <manaliparab10@gmail.com>
2020-10-19 10:56:41 +0530

    Used Random Function to check employee presence

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,5 +3,10 @@ public class EmployeeWageComputation {
    public static void main(String args[]){
 
       System.out.println("Welcome to Employee Wage Computation");
+      double empCheck=Math.floor(Math.random()*10)%2;
+      if (empCheck == 1 )
+          System.out.println("Employee is Present");
+      else
+          System.out.println("Employee is Absent");
    }
 }
